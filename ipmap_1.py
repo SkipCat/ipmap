e214e2ffffaf98d9e28067acb728b17d784516f4
Skipcat <cecile.huguet@supinternet.fr>
2017-11-27 12:32:14 +0100

    fixed loop error

--- ipmap_1.py
+++ ipmap_1.py
@@ -5,14 +5,10 @@ async def handle(request):
     ip = request.match_info.get('ip', "Anonymous")
     text = "IP: " + ip
 
-    create = asyncio.create_subprocess_shell('nmap -sV ' + ip)    
-    loop = asyncio.get_event_loop()
-    proc = loop.run_until_complete(create)
+    await asyncio.create_subprocess_shell('nmap -sV ' + ip)
 
-    exitcode = loop.run_until_complete(proc.wait())
-    assertEqual(exitcode, 7)
+    return web.Response(text = text)
 
-    return web.Response(text = exitcode)
 
 def main():
     app = web.Application()
