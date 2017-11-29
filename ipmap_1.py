c0a0661c7d7525794918979c5980a632928165b7
Skipcat <cecile.huguet@supinternet.fr>
2017-11-29 10:21:14 +0100

    improved handling of single scan

--- ipmap_1.py
+++ ipmap_1.py
@@ -1,22 +1,26 @@
-from aiohttp import web
 import asyncio
+from aiohttp import web
 
 async def handle(request):
-    ip = request.match_info.get('ip', "Anonymous")
-    text = "IP: " + ip
-    print(text)
+    ip = request.match_info.get('ip')
+    print("nmap -sV " + ip)
 
-    process = await asyncio.create_subprocess_shell(
-        'nmap -sV ' + ip,
+    process = await asyncio.create_subprocess_exec(
+        'nmap', '-sV', ip,
         stdout = asyncio.subprocess.PIPE
     )
-    stdout, stderr = await process.communicate()
+
+    stdout, _ = await process.communicate()
     result = stdout.decode().strip()
 
     return web.Response(text = result)
 
 
 def main():
+    # Sur Windows décommenter les 2 lignes ci-dessous :
+    # loop = asyncio.ProactorEventLoop()
+    # asyncio.set_event_loop(loop)
+
     app = web.Application()
     app.router.add_get('/{ip}', handle)
     web.run_app(app)
